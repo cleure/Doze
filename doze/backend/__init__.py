25a601c98be98060e359dc163e41877771d1cf2d
Cameron Eure <cleure@websprockets.com>
2011-07-27 19:00:42 -0700

    Added .gitignore rule for *.pyc Added __init__.py for doze.backend Added TODO file

--- doze/backend/__init__.py
+++ doze/backend/__init__.py
@@ -1,9 +1,6 @@
 
 import inspect
-from doze import *
-from backend import *
+from generic import *
 
 __all__ = [name for name, ref in locals().items()\
     if not name[0] == '_' and not inspect.ismodule(ref)]
-
-__version__ = '0.0.1'
